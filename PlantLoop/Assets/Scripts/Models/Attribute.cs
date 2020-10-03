c58cc19feccc1d155e17917b4ee7369e3582004c
panoramix360 <lucasreis3000@gmail.com>
2020-10-03 11:52:33 -0300

    creating attributes

---
+++ PlantLoop/Assets/Scripts/Models/Attribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Attribute
+{
+    [SerializeField] private AttributeEnum type;
+    [SerializeField] private double value;
+    [SerializeField] private double modifier;
+}
