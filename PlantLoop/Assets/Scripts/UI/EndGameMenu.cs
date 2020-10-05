6a662b11e0c48c733bf599fb2e647300da0aa9b9
Lucas Reis <lucasreis3000@gmail.com>
2020-10-05 08:40:54 -0300

    end game popup

---
+++ PlantLoop/Assets/Scripts/UI/EndGameMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameMenu : MonoBehaviour
+{
+    public void OnClickReplay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+}
