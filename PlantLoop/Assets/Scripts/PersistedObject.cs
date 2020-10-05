1d59b1f24a91c63add923777667156908e70328c
Lucas Reis <lucasreis3000@gmail.com>
2020-10-05 18:10:48 -0300

    finishing new cycle

---
+++ PlantLoop/Assets/Scripts/PersistedObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistedObject : Singleton<PersistedObject>
+{
+    public bool isChanged = false;
+
+    public float baseHp;
+    public float baseWater;
+    public float baseEnergy;
+
+    public float baseHpUnitPerTime;
+    public float baseWaterUnitPerTime;
+    public float baseEnergyUnitPerTime;
+}
